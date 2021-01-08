0a076baec1c6ddb6e695682d54cf2124bf222d09
arsalanwahid <arslanwahid1993@gmail.com>
2021-01-09 00:37:33 +0500

    initial commit with UI

---
+++ capital cities/capital cities/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  capital cities
+//
+//  Created by Arsalan Wahid Asghar on 09/01/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
